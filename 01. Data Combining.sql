348a8f3470d67ddd6e31299ed9ab66e0cc54602c
keniasaa <157902229+keniasaa@users.noreply.github.com>
2024-02-21 11:31:46 +0700

    Create 01. Data Combining.sql

---
+++ 01. Data Combining.sql
@@ -0,0 +1,25 @@
+CREATE TABLE gda-capstone-01.cyclistic23.combined AS (
+SELECT * FROM gda-capstone-01.cyclistic23.202301
+UNION ALL
+SELECT * FROM gda-capstone-01.cyclistic23.202302
+UNION ALL
+SELECT * FROM gda-capstone-01.cyclistic23.202303
+UNION ALL
+SELECT * FROM gda-capstone-01.cyclistic23.202304
+UNION ALL
+SELECT * FROM gda-capstone-01.cyclistic23.202305
+UNION ALL
+SELECT * FROM gda-capstone-01.cyclistic23.202306
+UNION ALL
+SELECT * FROM gda-capstone-01.cyclistic23.202307
+UNION ALL
+SELECT * FROM gda-capstone-01.cyclistic23.202308
+UNION ALL
+SELECT * FROM gda-capstone-01.cyclistic23.202309
+UNION ALL
+SELECT * FROM gda-capstone-01.cyclistic23.202310
+UNION ALL
+SELECT * FROM gda-capstone-01.cyclistic23.202311
+UNION ALL
+SELECT * FROM gda-capstone-01.cyclistic23.202312
+);
