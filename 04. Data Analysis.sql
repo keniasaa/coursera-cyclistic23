65da28afc204736ac828b20e76646989c3c85b62
keniasaa <157902229+keniasaa@users.noreply.github.com>
2024-02-21 12:10:25 +0700

    Update 04. Data Analysis.sql

--- 04. Data Analysis.sql
+++ 04. Data Analysis.sql
@@ -1,21 +1,21 @@
 -- Bikes types used by riders
 SELECT member_casual, rideable_type, 
 COUNT(*) AS total_rides
-FROM gda-capstone-01.cyclistic23.cleaned
+FROM gda-capstone-01.cyclistic23.cleaned23
 GROUP BY member_casual, rideable_type
 ORDER BY member_casual, total_rides;
 
 -- No. of rides per month
 SELECT member_casual, month, 
 COUNT(*) AS total_rides_per_month
-FROM gda-capstone-01.cyclistic23.cleaned
+FROM gda-capstone-01.cyclistic23.cleaned23
 GROUP BY member_casual, month
 ORDER BY member_casual,total_rides_per_month;
 
 -- No. of rides per day of week
 SELECT member_casual, day_of_week, 
 COUNT(*) AS total_rides_per_week
-FROM gda-capstone-01.cyclistic23.cleaned
+FROM gda-capstone-01.cyclistic23.cleaned23
 GROUP BY member_casual, day_of_week
 ORDER BY member_casual, total_rides_per_week;
 
@@ -23,26 +23,26 @@ ORDER BY member_casual, total_rides_per_week;
 SELECT member_casual, 
 EXTRACT(HOUR FROM started_at) AS hour_of_day, 
 COUNT(*) AS total_rides_hour
-FROM gda-capstone-01.cyclistic23.cleaned
+FROM gda-capstone-01.cyclistic23.cleaned23
 GROUP BY member_casual,hour_of_day
 ORDER BY member_casual, total_rides_hour;
 
 -- Average ride_length per month
-SELECT member_casual, month, AVG(ride_length_in_mins) AS avg_ride_month
-FROM gda-capstone-01.cyclistic23.cleaned
+SELECT member_casual, month, AVG(ride_length) AS avg_ride_month
+FROM gda-capstone-01.cyclistic23.cleaned23
 GROUP BY member_casual, month
 ORDER BY member_casual, avg_ride_month;
 
 -- Average ride_length per day of week
-SELECT member_casual, day_of_week, AVG(ride_length_in_mins) AS avg_ride_week
-FROM gda-capstone-01.cyclistic23.cleaned
+SELECT member_casual, day_of_week, AVG(ride_length) AS avg_ride_week
+FROM gda-capstone-01.cyclistic23.cleaned23
 GROUP BY member_casual, day_of_week
 ORDER BY member_casual, avg_ride_week;
 
 
 -- Average ride_length per hour
-SELECT member_casual, EXTRACT(HOUR FROM started_at) AS hour_of_day, AVG(ride_length_in_mins) AS avg_ride_hour
-FROM gda-capstone-01.cyclistic23.cleaned
+SELECT member_casual, EXTRACT(HOUR FROM started_at) AS hour_of_day, AVG(ride_length) AS avg_ride_hour
+FROM gda-capstone-01.cyclistic23.cleaned23
 GROUP BY member_casual, hour_of_day
 ORDER BY member_casual, avg_ride_hour;
 
@@ -51,7 +51,7 @@ ORDER BY member_casual, avg_ride_hour;
 SELECT member_casual, start_station_name, 
   AVG(start_lat) AS start_lat, AVG(start_lng) AS start_lng,
   COUNT(ride_id) AS total_rides
-FROM gda-capstone-01.cyclistic23.cleaned
+FROM gda-capstone-01.cyclistic23.cleaned23
 GROUP BY start_station_name, member_casual
 ORDER BY member_casual;
 
@@ -59,6 +59,6 @@ ORDER BY member_casual;
 SELECT member_casual,end_station_name, 
   AVG(end_lat) AS end_lat, AVG(end_lng) AS end_lng,
   COUNT(ride_id) AS total_rides
-FROM gda-capstone-01.cyclistic23.cleaned
+FROM gda-capstone-01.cyclistic23.cleaned23
 GROUP BY end_station_name, member_casual
 ORDER BY member_casual;
